5d18254409a45b922b632ff6c45ffabb24597931
Elisha Okon <eliscodavid@gmail.com>
2023-07-21 00:22:45 +0100

    Update totalPayout.jsx

--- components/Read Contract/totalPayout.jsx
+++ components/Read Contract/totalPayout.jsx
@@ -1,26 +1,34 @@
 import { useAddress, useContract, useContractRead } from "@thirdweb-dev/react";
-import { useState, useEffect} from 'react'
+import { useState, useEffect } from 'react';
 import { ethers } from "ethers";
+
 function TotalPayout() {
-    const address = useAddress(); 
+    const address = useAddress();
     const { contract } = useContract("0x593649F70f836565e33f0BCe9af9503c243359B3");
-    const { data:balance, refetch: refetchBalance, isLoading } = useContractRead(contract, "getTotalDividendsDistributed",);
+    const { data: balance, refetch: refetchBalance, isLoading } = useContractRead(contract, "getTotalDividendsDistributed");
 
     useEffect(() => {
         setInterval(() => {
-          refetchData();
+            refetchData();
         }, 10000);
-      }, []);
+    }, []);
 
-      const refetchData = () => { refetchBalance()}
-  
+    const refetchData = () => {
+        refetchBalance();
+    };
+
     if (isLoading) {
-      return <div>Loading...</div>;
+        return <div>Loading...</div>;
     }
-  
-    return <div><span id='balance'>
-    {balance && ethers.utils.formatEther(Number(balance.toFixed(2))}
-    </span></div>;
-  }
 
-  export default TotalPayout;
+    // Adding a closing parenthesis ')' after Number(balance.toFixed(2))
+    return (
+        <div>
+            <span id='balance'>
+                {balance && ethers.utils.formatEther(Number(balance.toFixed(2)))}
+            </span>
+        </div>
+    );
+}
+
+export default TotalPayout;
