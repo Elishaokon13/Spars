9bcde32ce6c6ec863d94d2fe09eefce5e5a5a4a2
Elisha Okon <eliscodavid@gmail.com>
2023-07-22 23:02:47 +0100

    Update totalPayout.jsx

--- components/Read Contract/totalPayout.jsx
+++ components/Read Contract/totalPayout.jsx
@@ -1,30 +1,38 @@
 import { useAddress, useContract, useContractRead } from "@thirdweb-dev/react";
-import { useState, useEffect} from 'react'
+import { useState, useEffect } from 'react';
 import { ethers } from "ethers";
+
 function TotalPayout() {
-    const address = useAddress(); 
-    const { contract } = useContract("0x593649F70f836565e33f0BCe9af9503c243359B3");
-    const { data:balance, refetch: refetchBalance, isLoading } = useContractRead(contract, "getTotalDividendsDistributed",);
+  const address = useAddress(); 
+  const { contract } = useContract("0x593649F70f836565e33f0BCe9af9503c243359B3");
+  const { data: balance, refetch: refetchBalance, isLoading } = useContractRead(contract, "getTotalDividendsDistributed");
 
-    useEffect(() => {
-        setInterval(() => {
-          refetchData();
-        }, 10000);
-      }, []);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      refetchData();
+    }, 10000);
 
-      const refetchData = () => { refetchBalance()}
-  
-    if (isLoading) {
-      return <div>Loading...</div>;
-    }
-  
-    return (
-  <div>
-    <span id='balance'>
-      {balance && Number(ethers.utils.formatEther(balance.toString())).toFixed(2)}
-    </span>
-  </div>
-);
-    
+    // Clear the interval when the component is unmounted
+    return () => clearInterval(intervalId);
+  }, []);
 
-  export default TotalPayout;
+  const refetchData = () => {
+    refetchBalance();
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  const formattedBalance = balance ? Number(ethers.utils.formatEther(balance.toString())).toFixed(2) : "N/A";
+
+  return (
+    <div>
+      <span id='balance'>
+        {formattedBalance}
+      </span>
+    </div>
+  );
+}
+
+export default TotalPayout;
