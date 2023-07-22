030fd393faf8f7742fb0b78a967de2a6e620d439
Elisha Okon <eliscodavid@gmail.com>
2023-07-22 23:23:13 +0100

    Update totalPayout.jsx

--- components/Read Contract/totalPayout.jsx
+++ components/Read Contract/totalPayout.jsx
@@ -2,6 +2,8 @@ import { useAddress, useContract, useContractRead } from "@thirdweb-dev/react";
 import { useState, useEffect } from 'react';
 import { ethers } from "ethers";
 import thunderBolt from '../assets/thunder-bolt1.png'
+import Image from 'next/image';
+
 
 function TotalPayout() {
   const address = useAddress(); 
