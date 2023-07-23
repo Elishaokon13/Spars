9483b4bdc2be71c374e4d9061e7316db06e23792
Elisha Okon <eliscodavid@gmail.com>
2023-07-23 19:25:37 +0000

    update

--- components/Read Contract/getAccountDividendInfo.jsx
+++ components/Read Contract/getAccountDividendInfo.jsx
@@ -1,38 +1,18 @@
 import { useContract, useContractRead } from "@thirdweb-dev/react";
-import { useState, useEffect } from 'react'
-import { ethers } from "ethers";
+import { useAddress } from "@thirdweb-dev/react";
 import thunderBolt from '../../assets/thunder-bolt1.png'
 import Image from 'next/image';
 
-const GetAccountDividendInfo = () => {
-  const { contract } = useContract("0x593649F70f836565e33f0BCe9af9503c243359B3");
-  const { data: balance, refetch: refetchBalance, isLoading } = useContractRead(contract, "withdrawableDividendOf")
+export default function WithdrawableDividendOf () {
+    const address = useAddress();
+    const { contract } = useContract("0x593649F70f836565e33f0BCe9af9503c243359B3");
+    const { data, isLoading } = useContractRead(contract, "withdrawableDividendOf", [address])
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      refetchData();
-    }, 10000);
+    if (isLoading) {
+        return <Image className='object-cover thunder-bolt' alt='img' src={thunderBolt} />;
+    }
 
-    // Clean up the interval on component unmount
-    return () => clearInterval(interval);
-  }, []);
+    //const balance = data; 
 
-  const refetchData = () => { refetchBalance() }
-
-  if (isLoading) {
-    return <Image className='object-cover thunder-bolt' alt='img' src={thunderBolt} />;
+    return <div>{data}</div>;
   }
-
-  // Display the uint256 value from the balance array at index 3
-  const uintValue = balance ? balance[3].toString() : 'N/A';
-
-  return (
-    <div>
-      <span id='balance'>
-        {uintValue} PARS
-      </span>
-    </div>
-  );
-}
-
-export default GetAccountDividendInfo;
