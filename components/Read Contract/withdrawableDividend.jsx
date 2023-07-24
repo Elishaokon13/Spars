5d8ba4cfc5ab7d3b4092fdbe676c7840db446bf2
Elisha Okon <eliscodavid@gmail.com>
2023-07-24 22:23:02 +0000

    update withdrawable

--- components/Read Contract/withdrawableDividend.jsx
+++ components/Read Contract/withdrawableDividend.jsx
@@ -3,29 +3,29 @@ import { useAddress } from "@thirdweb-dev/react";
 import { useEffect, useState } from "react";
 
 export default function WithdrawableDividend({ customAddress }) {
-  const address = customAddress || useAddress();
-  const { contract } = useContract("0x593649F70f836565e33f0BCe9af9503c243359B3");
-  const { data: balance, refetch: refetchBalance, isLoading, error } = useContractRead(contract, "withdrawableDividendOf", [address]);
+    const address = customAddress || useAddress();
+    const { contract } = useContract("0x593649F70f836565e33f0BCe9af9503c243359B3");
+    const { data: balance, refetch: refetchBalance, isLoading, error } = useContractRead(contract, "withdrawableDividendOf", [address]);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      refetchData();
-    }, 10000);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            refetchData();
+        }, 10000);
 
-    return () => clearInterval(interval);
-  }, [address]);
+        return () => clearInterval(interval);
+    }, [address, refetchData]);
 
-  const refetchData = () => {
-    refetchBalance();
-  };
+    const refetchData = () => {
+        refetchBalance();
+    };
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
 
-  return <div>{balance ? balance.toString() : 'N/A'}</div>;
+    return <div>{balance ? balance.toString() : 'N/A'}</div>;
 }
