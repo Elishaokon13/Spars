fa8ca4a15d4b3ab3d614e1062e3bfdc7c1db3d75
Elisha Okon <eliscodavid@gmail.com>
2023-07-24 21:31:34 +0000

    deployyed an update on withdrawable

--- components/Read Contract/withdrawableDividend.jsx
+++ components/Read Contract/withdrawableDividend.jsx
@@ -1,19 +1,27 @@
 import { useContract, useContractRead } from "@thirdweb-dev/react";
 import { useAddress } from "@thirdweb-dev/react";
-import thunderBolt from '../../assets/thunder-bolt1.png';
-import Image from 'next/image';
+import { useEffect } from "react";
 
 export default function WithdrawableDividend() {
-    const address = useAddress();
-    const contractAddress = "0x593649F70f836565e33f0BCe9af9503c243359B3";
-    const { contract } = useContract(contractAddress);
-    const { data, isLoading } = useContractRead(contract, "withdrawableDividendOf", [address]);
+  const address = useAddress();
+  const { contract } = useContract("0x593649F70f836565e33f0BCe9af9503c243359B3");
+  const { data: balance, refetch: refetchBalance, isLoading } = useContractRead(contract, "withdrawableDividendOf");
 
-    if (isLoading) {
-        return <Image className='object-cover thunder-bolt' alt='img' src={thunderBolt} />;
-    }
+  useEffect(() => {
+    const interval = setInterval(() => {
+      refetchData();
+    }, 10000);
 
-    //const balance = data;
+    return () => clearInterval(interval);
+  }, []);
 
-    return <div>{data}</div>;
+  const refetchData = () => {
+    refetchBalance();
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return <div>{balance ? balance.toString() : 'N/A'}</div>;
 }
