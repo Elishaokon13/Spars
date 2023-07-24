e70ff8b6185cc651fecc65444eceaa7153df13c0
Elisha Okon <eliscodavid@gmail.com>
2023-07-24 22:17:33 +0000

    updated claim

--- components/Read Contract/withdrawableDividend.jsx
+++ components/Read Contract/withdrawableDividend.jsx
@@ -1,11 +1,11 @@
 import { useContract, useContractRead } from "@thirdweb-dev/react";
 import { useAddress } from "@thirdweb-dev/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-export default function WithdrawableDividend() {
-  const address = useAddress();
+export default function WithdrawableDividend({ customAddress }) {
+  const address = customAddress || useAddress();
   const { contract } = useContract("0x593649F70f836565e33f0BCe9af9503c243359B3");
-  const { data: balance, refetch: refetchBalance, isLoading } = useContractRead(contract, "withdrawableDividendOf");
+  const { data: balance, refetch: refetchBalance, isLoading, error } = useContractRead(contract, "withdrawableDividendOf", [address]);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -13,7 +13,7 @@ export default function WithdrawableDividend() {
     }, 10000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [address]);
 
   const refetchData = () => {
     refetchBalance();
@@ -21,6 +21,10 @@ export default function WithdrawableDividend() {
 
   if (isLoading) {
     return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
   }
 
   return <div>{balance ? balance.toString() : 'N/A'}</div>;
