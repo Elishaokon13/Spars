0a2572e0359e6949d8f99b75d92ab711237658e4
Elisha Okon <eliscodavid@gmail.com>
2023-07-24 22:29:26 +0000

    At this point God Abeg

--- components/Read Contract/withdrawableDividend.jsx
+++ components/Read Contract/withdrawableDividend.jsx
@@ -1,31 +1,31 @@
 import { useContract, useContractRead } from "@thirdweb-dev/react";
 import { useAddress } from "@thirdweb-dev/react";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 
 export default function WithdrawableDividend({ customAddress }) {
-    const address = customAddress || useAddress();
-    const { contract } = useContract("0x593649F70f836565e33f0BCe9af9503c243359B3");
-    const { data: balance, refetch: refetchBalance, isLoading, error } = useContractRead(contract, "withdrawableDividendOf", [address]);
+  const address = customAddress || useAddress();
+  const { contract } = useContract("0x593649F70f836565e33f0BCe9af9503c243359B3");
+  const { data: balance, refetch: refetchBalance, isLoading, error } = useContractRead(contract, "withdrawableDividendOf", [address]);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            refetchData();
-        }, 10000);
+  const refetchData = useCallback(() => {
+    refetchBalance();
+  }, [refetchBalance]);
 
-        return () => clearInterval(interval);
-    }, [address, refetchData]);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      refetchData();
+    }, 10000);
 
-    const refetchData = () => {
-        refetchBalance();
-    };
+    return () => clearInterval(interval);
+  }, [refetchData]);
 
-    if (isLoading) {
-        return <div>Loading...</div>;
-    }
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
-    if (error) {
-        return <div>Error: {error.message}</div>;
-    }
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
 
-    return <div>{balance ? balance.toString() : 'N/A'}</div>;
+  return <div>{balance ? balance.toString() : 'N/A'}</div>;
 }
