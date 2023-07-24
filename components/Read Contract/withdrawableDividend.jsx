453136d1fa58d8e5631266b4a64fd1fd23475a28
Elisha Okon <eliscodavid@gmail.com>
2023-07-24 21:03:12 +0000

    update

--- components/Read Contract/withdrawableDividend.jsx
+++ components/Read Contract/withdrawableDividend.jsx
@@ -1,18 +1,19 @@
 import { useContract, useContractRead } from "@thirdweb-dev/react";
 import { useAddress } from "@thirdweb-dev/react";
-import thunderBolt from '../../assets/thunder-bolt1.png'
+import thunderBolt from '../../assets/thunder-bolt1.png';
 import Image from 'next/image';
 
 export default function WithdrawableDividend() {
     const address = useAddress();
-    const { contract } = useContract("0x593649F70f836565e33f0BCe9af9503c243359B3");
-    const { data, isLoading } = useContractRead(contract, "withdrawableDividendOf", [address])
+    const contractAddress = "0x593649F70f836565e33f0BCe9af9503c243359B3";
+    const { contract } = useContract(contractAddress);
+    const { data, isLoading } = useContractRead(contract, "withdrawableDividendOf", [address]);
 
     if (isLoading) {
         return <Image className='object-cover thunder-bolt' alt='img' src={thunderBolt} />;
     }
 
-    //const balance = data; 
+    //const balance = data;
 
     return <div>{data}</div>;
-  }
+}
